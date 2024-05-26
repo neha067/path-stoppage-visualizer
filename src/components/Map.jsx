345c6a45d2bb196f5570b4021972d677f130de19
neha067 <nehapriyaekka2000@gmail.com>
2024-05-26 11:32:31 +0530

    add: stoppage-info

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { MapContainer, Marker, TileLayer, Polyline } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Polyline,
+  Tooltip,
+} from "react-leaflet";
 import osm from "./osm-providers";
-import "./Map.module.css";
+import styles from "./Map.module.css";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import Papa from "papaparse";
@@ -20,60 +27,202 @@ const stopIcon = new L.Icon({
 });
 
 const MapComponent = () => {
-  const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
-  const [zoom, setZoom] = useState(10);
+  const [center, setCenter] = useState({ lat: 12.9294916, lng: 74.9173533 });
+  const [zoom, setZoom] = useState(11);
   const [path, setPath] = useState([]);
-  const [markerPosition, setMarkerPosition] = useState([13.084622, 80.248357]);
+  const [movingIconPosition, setmovingIconPosition] = useState([
+    13.084622, 80.248357,
+  ]);
   const [stops, setStops] = useState([]);
+  const [tooltipContent, setTooltipContent] = useState("");
+
+  const [threshold, setThreshold] = useState(0);
+
+  const handleInputChange = (event) => {
+    if (isNaN(Number(event.target.value))) {
+      return;
+    }
+    setThreshold(event.target.value);
+    setPath([]);
+    setStops([]);
+    setmovingIconPosition([13.084622, 80.248357]);
+    console.log("Threshold", threshold);
+  };
 
   {
     path.length > 0 && <Polyline positions={path} color="red" />;
   }
 
   useEffect(() => {
+    if (
+      Number(threshold) === 0 ||
+      threshold === "" ||
+      Number(threshold) === NaN
+    ) {
+      setmovingIconPosition([13.084622, 80.248357]);
+      setPath([]);
+      setStops([]);
+    }
+  }, [threshold]);
+
+  useEffect(() => {
     const data = require("../assets/csvjson.json");
     const parsedPath = data.map((item) => [item.latitude, item.longitude]);
     setPath(parsedPath);
-    const stopLocations = data.filter((item) => item.speed === 0);
-    setStops(stopLocations);
-  }, []);
+
+    // Sort data by eventGeneratedTime
+    const sortedData = [...data].sort(
+      (a, b) => a.eventGeneratedTime - b.eventGeneratedTime
+    );
+
+    const stoppages = [];
+    const thresholdCheck = Number(threshold) * 60 * 1000;
+    let i = 0;
+    while (i < sortedData.length - 1) {
+      if (sortedData[i].speed === 0) {
+        // Find the next data point where the speed is not 0
+        let j = i + 1;
+        while (j < sortedData.length && sortedData[j].speed === 0) {
+          j++;
+        }
+
+        // If we found a data point where the speed is not 0
+        if (j < sortedData.length) {
+          // Calculate the waiting time
+          const waitingTime =
+            sortedData[j].eventGeneratedTime - sortedData[i].eventGeneratedTime;
+
+          // Check if the waiting time exceeds the stoppage threshold
+          if (waitingTime > thresholdCheck) {
+            // Add the waiting time to the stop
+            const stop = {
+              ...sortedData[i],
+              waitingTime,
+            };
+
+            stoppages.push(stop);
+          }
+
+          // Start the next iteration from the new stop
+          i = j;
+        } else {
+          // If we didn't find a data point where the speed is not 0, break the loop
+          break;
+        }
+      } else {
+        i++;
+      }
+    }
+
+    setStops(stoppages);
+  }, [threshold]);
 
   // Move marker along the path
   useEffect(() => {
-    const interval = setInterval(() => {
+    let interval;
+
+    const moveMarker = () => {
       if (path.length > 0) {
-        setMarkerPosition(path.shift());
+        const nextPosition = path.shift();
+        setmovingIconPosition(nextPosition);
         setPath(path);
+
+        // Check if the next position is a stop
+        if (
+          stops.some(
+            (stop) =>
+              stop.latitude === nextPosition[0] &&
+              stop.longitude === nextPosition[1]
+          )
+        ) {
+          const stopInfo = stops.find(
+            (stop) =>
+              stop.latitude === nextPosition[0] &&
+              stop.longitude === nextPosition[1]
+          );
+          setTooltipContent(`${stopInfo.EquipmentId}`);
+          clearInterval(interval); // Clear the interval
+          setTimeout(() => {
+            interval = setInterval(moveMarker, 100); // Restart the interval after a minute
+          }, 1000);
+        }
       }
-    }, 100); // Move every second
+    };
+
+    interval = setInterval(moveMarker, 10); // Move every second
 
     return () => clearInterval(interval);
-  }, [path]);
+  }, [path, stops]);
 
   return (
-    <div className="leaflet-container">
-      <MapContainer
-        center={center}
-        zoom={zoom}
-        style={{ height: "100vh", width: "100%" }}
-      >
-        <TileLayer
-          url={osm.maptiler.url}
-          attribution={osm.maptiler.attribution}
-        />
-        <Polyline positions={path} color="red" />
-        {markerPosition && (
-          <Marker position={markerPosition} icon={movingIcon} />
-        )}
-        {stops.map((stop, index) => (
-          <Marker
-            key={index}
-            position={[stop.latitude, stop.longitude]}
-            icon={stopIcon}
-          />
-        ))}
-      </MapContainer>
-    </div>
+    <>
+      <div>
+        <div className={styles.container}>
+          <div className={styles.threshold}>
+            <h1>Threshold (in minutes):</h1>
+            <div>
+              <input
+                type="number"
+                value={threshold}
+                onChange={handleInputChange}
+                placeholder="Enter the threshold value"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="leaflet-container">
+          <MapContainer
+            center={center}
+            zoom={zoom}
+            style={{
+              height: "60vh",
+              width: "94%",
+              margin: "3rem 3rem 3rem 3rem",
+            }}
+          >
+            <TileLayer
+              url={osm.maptiler.url}
+              attribution={osm.maptiler.attribution}
+            />
+            <Polyline positions={path} color="red" />
+            {movingIconPosition && (
+              <Marker position={movingIconPosition} icon={movingIcon}>
+                <Tooltip permanent>{tooltipContent}</Tooltip>
+              </Marker>
+            )}
+            {stops.map((stop, index, array) => {
+              const previousStop = array[index - 1];
+              const stoppageDuration = previousStop
+                ? (stop.eventGeneratedTime - previousStop.eventGeneratedTime) /
+                  60000
+                : 0;
+
+              return (
+                <Marker
+                  key={index}
+                  position={[stop.latitude, stop.longitude]}
+                  icon={stopIcon}
+                >
+                  <Popup>
+                    Equipment ID: {stop.EquipmentId}
+                    <br />
+                    Reach Time:{" "}
+                    {new Date(stop.eventGeneratedTime).toLocaleString()}
+                    <br />
+                    End Time:{" "}
+                    {new Date(
+                      stop.eventGeneratedTime + stoppageDuration * 60000
+                    ).toLocaleString()}
+                    <br />
+                    Stoppage Duration: {stoppageDuration} minutes
+                  </Popup>
+                </Marker>
+              );
+            })}
+          </MapContainer>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/PopupInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PopupInfo = () => {
+  return <div>PopupInfo</div>;
+};
+
+export default PopupInfo;
